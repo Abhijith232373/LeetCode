529b82f03195cf763a16556e498b4bbb71b8261c
Abhijith P.R <165657160+Abhijith232373@users.noreply.github.com>
2025-07-28 21:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 58.2 MB (64.63%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    return nums.concat(nums);
+};
+getConcatenation([1,2,1])
+getConcatenation([1,3,2,1])
