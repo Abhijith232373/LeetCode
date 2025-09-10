ecf59ecfd86752f79130005daad93c1cd6b6dc8c
Abhijith P.R <abhijithpr366@gmail.com>
2025-09-10 21:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (6.84%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const splited=s.trim().split(" ")
+    return splited[splited.length-1].length;
+}
+lengthOfLastWord("  Hello World  ")
